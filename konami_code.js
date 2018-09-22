7586902f33427b68b7b62acd8c28761ba3a8faaf
beneficial-bundle-8122 <satyasinha94@gmail.com>
2018-09-22 21:16:23 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -12,4 +12,21 @@ const codes = [
 ];
 
 function init() {
-}
+  let index = 0;
+  document.body.addEventListener('keydown', function (e) {
+  const key=e.key
+  if(key===codes[index]) {
+    console.log(key)
+    console.log(index)
+    index++;
+    if(index===codes.length){
+      alert("Konami Code activated");
+      console.log("Konami Code activated");
+      index=0
+    }
+  }
+      else {
+        index=0    
+      }
+    }
+    )}
